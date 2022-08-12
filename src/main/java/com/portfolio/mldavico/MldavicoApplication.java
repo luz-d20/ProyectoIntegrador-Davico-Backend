4aa103a3fe644e3cf0da98cf749b6ae0155b58ec
Luz Davico <mldavico@gmail.com>
2022-08-11 21:42:12 -0300

    Initial commit

---
+++ src/main/java/com/portfolio/mldavico/MldavicoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mldavico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MldavicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MldavicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/mldavico/MldavicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mldavico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MldavicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
