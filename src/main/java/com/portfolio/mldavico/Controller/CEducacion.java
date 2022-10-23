467a7aebf5f93121eb2c04f651718b526d174f34
Luz Davico <mldavico@gmail.com>
2022-10-23 20:32:21 -0300

    Completed backend cofig

---
+++ src/main/java/com/portfolio/mldavico/Controller/CEducacion.java
@@ -0,0 +1,84 @@
+package com.portfolio.mldavico.Controller;
+
+import com.portfolio.mldavico.Dto.dtoEducacion;
+import com.portfolio.mldavico.Entity.Educacion;
+import com.portfolio.mldavico.Security.Controller.Mensaje;
+import com.portfolio.mldavico.Service.SEducacion;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("educacion")
+@CrossOrigin(origins = "http://localhost:4200")
+
+public class CEducacion {
+    @Autowired
+    SEducacion sEducacion;
+    
+    @GetMapping("/lista")
+    public ResponseEntity<List<Educacion>> list(){
+        List<Educacion> list = sEducacion.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        if (!sEducacion.existsById(id)) {
+            return new ResponseEntity(new Mensaje("ID inexistente"), HttpStatus.NOT_FOUND);
+        }
+        sEducacion.delete(id);
+        return new ResponseEntity(new Mensaje("Entrada eliminada"), HttpStatus.OK);
+    }
+    
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Educacion> getById(@PathVariable("id") int id){
+        if(!sEducacion.existsById(id))
+            return new ResponseEntity(new Mensaje("ID inexistente"), HttpStatus.NOT_FOUND);
+        Educacion educacion = sEducacion.getOne(id).get();
+        return new ResponseEntity(educacion, HttpStatus.OK);
+    }
+
+    
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody dtoEducacion dtoedu){      
+        if(StringUtils.isBlank(dtoedu.getNombreE()))
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        if(sEducacion.existsByNombreE(dtoedu.getNombreE()))
+            return new ResponseEntity(new Mensaje("Entrada ya existente"), HttpStatus.BAD_REQUEST);
+        
+        Educacion educacion = new Educacion (dtoedu.getNombreE(), dtoedu.getDescripcionE());
+        sEducacion.save(educacion);
+        
+        return new ResponseEntity(new Mensaje("Entrada añadida"), HttpStatus.OK);
+    }
+    
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoEducacion dtoedu){
+        if(!sEducacion.existsById(id))
+            return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
+        if(sEducacion.existsByNombreE(dtoedu.getNombreE()) && sEducacion.getByNombreE(dtoedu.getNombreE()).get().getId() != id)
+            return new ResponseEntity(new Mensaje("Entrada ya existente"), HttpStatus.BAD_REQUEST);
+        if(StringUtils.isBlank(dtoedu.getNombreE()))
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        
+        Educacion educacion = sEducacion.getOne(id).get();
+        educacion.setNombreE(dtoedu.getNombreE());
+        educacion.setDescripcionE((dtoedu.getDescripcionE()));
+        
+        sEducacion.save(educacion);
+        return new ResponseEntity(new Mensaje("Entrada actualizada"), HttpStatus.OK);
+             
+    }
+}
---
+++ src/main/java/com/portfolio/mldavico/Security/Service/RolService.java
@@ -0,0 +1,24 @@
+package com.portfolio.mldavico.Security.Service;
+
+import com.portfolio.mldavico.Security.Entity.Rol;
+import com.portfolio.mldavico.Security.Repository.iRolRepository;
+import com.portfolio.mldavico.Security.Enums.RolNombre;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository iRolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre) {
+        return iRolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        iRolRepository.save(rol);
+    }
+}
