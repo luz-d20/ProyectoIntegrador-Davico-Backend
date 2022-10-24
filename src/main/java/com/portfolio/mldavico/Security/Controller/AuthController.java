cea2dd8c162d1ccd3ec09ee26cecb87664a03551
Luz Davico <mldavico@gmail.com>
2022-10-23 21:18:40 -0300

    Correccion de CORS

--- src/main/java/com/portfolio/mldavico/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/mldavico/Security/Controller/AuthController.java
@@ -40,7 +40,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://frontend-argprog-davico.web.app/")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
